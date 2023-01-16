ac26c0a66a6f2a98509eb5fb374520e3cdb44245
aryaa30 <82461930+aryaa30@users.noreply.github.com>
2023-01-16 05:44:14 -0800

    api

--- src/components/Customers/comments.js
+++ src/components/Customers/comments.js
@@ -22,7 +22,6 @@ axios
 
   return (
    <div className='carding'>
-   <h1> Axios api</h1>
  
    {myData.map((post)=>{
 const {ID, Name, rating, Reviews} = post;
@@ -32,8 +31,8 @@ return <div className='main__api'>
 <h2 className='name__1'>
   Name :   {Name}
 </h2>
-<p className='name__2'>Rating :  {rating}</p>
-<p className='name__3'>Reviews :  {Reviews.slice(0 ,150)}</p>
+<p className='rati__2'>Rating :  {rating}</p>
+<p className='revi__3'>Reviews :  {Reviews.slice(0 ,150)}</p>
 </div>
 
 </div>
